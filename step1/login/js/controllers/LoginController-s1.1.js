6d0a6a0cef47779bb66777fa2a1c2d34559ff8d8
CômeB <come.burguburu@gmail.com>
2015-10-21 15:28:25 +0200

    correct html , correct controller

--- step1/login/js/controllers/LoginController-s1.1.js
+++ step1/login/js/controllers/LoginController-s1.1.js
@@ -1,8 +1,8 @@
-angular.module('loginApp').controller('loginCtrl',loginCrtFnt);
+angular.module('loginApp',[]).controller('loginCtrl',loginCrtFnt);
 loginCrtFnt.$inject=['$scope','$log'];
 function loginCrtFnt($scope, $log){
-$scope.logAuth = function() {
-$log.info('user login', $scope.user.login);
-$log.info('user pwd', $scope.user.pwd);
-};
+	$scope.logAuth = function() {
+		$log.info('user login', $scope.user.login);
+		$log.info('user pwd', $scope.user.pwd);
+	};
 }
