b2fbbd42a75957e17980e272711ef6917fa0074b
CômeB <come.burguburu@gmail.com>
2015-10-21 15:52:01 +0200

    logAuth working

--- step1/login/js/controllers/LoginController-s1.1.js
+++ step1/login/js/controllers/LoginController-s1.1.js
@@ -2,7 +2,8 @@ angular.module('loginApp',[]).controller('loginCtrl',loginCrtFnt);
 loginCrtFnt.$inject=['$scope','$log'];
 function loginCrtFnt($scope, $log){
 	$scope.logAuth = function() {
-		$log.info('user login', $scope.user.login);
-		$log.info('user pwd', $scope.user.pwd);
+		$log.info('user login:', $scope.user.login);
+		$log.info('user pwd:', $scope.user.pwd);
 	};
+
 }
