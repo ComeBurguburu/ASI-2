6c4829b18b72e1620c111bc291b418aa9dbd9c75
CômeB <come.burguburu@gmail.com>
2015-10-21 16:18:22 +0200

    logAuthObject added

--- step1/login/js/controllers/LoginController-s1.1.js
+++ step1/login/js/controllers/LoginController-s1.1.js
@@ -5,5 +5,9 @@ function loginCrtFnt($scope, $log){
 		$log.info('user login:', $scope.user.login);
 		$log.info('user pwd:', $scope.user.pwd);
 	};
+	$scope.logAuthObject = function(user) {
+		$log.info('user object login:', user.login);
+		$log.info('user object pwd:', user.pwd);
+	};
 
 }
