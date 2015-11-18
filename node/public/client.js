505f7257c9e6bc0dbed315b01c0ddcd61eac203f
Youssef Gana <youssef.gana@mastertp>
2015-11-18 17:46:25 +0100

    correction client.js

--- node/public/client.js
+++ node/public/client.js
@@ -1,21 +1,7 @@
 /*global io*/
 var socket = io.connect();
-
 /**
  * Envoi d'un message
  */
 socket.emit('slidEvent','salut');
-<<<<<<< HEAD
-socket.on('open',function(){alert("ready");});
-socket.on('connection',function(){alert("ready");});
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 socket.on('connection',function(msg){alert(msg);});
-=======
-socket.on('connection',function(){alert("ready");});
->>>>>>> Stashed changes
-=======
-socket.on('connection',function(){alert("ready");});
->>>>>>> Stashed changes
->>>>>>> c5249e464dbd46c8312cd20300007d36e6dc02e1
